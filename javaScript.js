0cc552c7a273221e047141f870faac72367db660
fedgreco <effegi.044377@gmail.com>
2025-07-12 10:21:48 +0200

    somma i numeri

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,8 @@
 let numeri = [0, 1, 2, 3];
+let somma = 0;
 
-console.log(numeri);
+for (let i = 0; i < numeri.length; i++) {
+    somma += numeri[i];
+}
+
+console.log('La somma dei numeri è: ' + numeri.length);
