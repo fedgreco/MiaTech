31d53a87db3db87bb5c1ef4e9008343f5a4854aa
fedgreco <effegi.044377@gmail.com>
2025-07-12 10:28:49 +0200

    array reverse

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,4 @@
 let numeri = [0, 1, 2, 3];
-let somma = 0;
 
-for (let i = 0; i < numeri.length; i++) {
-    somma += numeri[i];
-}
-
-console.log('La somma dei numeri è: ' + numeri.length);
+numeri.reverse();
+console.log(numeri);
