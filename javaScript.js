08535c0e33d8cff8cfaba06e6cad3c82957539a1
fedgreco <effegi.044377@gmail.com>
2025-07-13 12:07:40 +0200

    reduce & sort

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,11 @@
-let students = [
-  { name: "Mario", grade: 75 },
-  { name: "Djana", grade: 45 },
-  { name: "Carlo", grade: 58 },
-  { name: "Dario", grade: 90 },
-  { name: "Stefano", grade: 69 }
-];
+let expenses = [33, 100, 25, 69, 9, 18, 45];
 
-let passedStudents = students.filter(student => student.grade >= 60);
+let totalExpenses = expenses.reduce((acc, curr) => acc + curr, 0);
 
-let firstFailedStudent = students.find(student => student.grade < 60);
+let words = ["banana", "apple", "cherry", "date"];
 
-console.log("Studenti promossi:", passedStudents);
-console.log("Primo studente bocciato:", firstFailedStudent);
+words.sort();
+
+console.log("Somma totale delle spese:", totalExpenses);
+console.log("Parole ordinate alfabeticamente:", words);
+
