5194618fd68ea3ecf0d683cdac7d43a35624ca10
fedgreco <effegi.044377@gmail.com>
2025-07-20 11:10:13 +0200

    aggiungi metodi & proprietà

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,24 @@
 class Automobile {
   constructor(marca, modello, anno) {
-    this.marca = marca;    // proprietà dell’oggetto
+    this.marca = marca;
     this.modello = modello;
     this.anno = anno;
+    this.chilometraggio = 0;
+  }
+
+  aggiungiChilometri(km) {
+    this.chilometraggio =+ km;
+  }
+
+  mostraChilometraggio() {
+    return `Chilometraggio: ${this.chilometraggio} km`;
   }
 
   descrizione() {
     return `Questa automobile è una ${this.marca} ${this.modello} del ${this.anno}.`;
   }
+
+
 }
+
+
