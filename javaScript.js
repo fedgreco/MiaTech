c8259da1430b7f0e943d9020ff4a3977adea693a
fedgreco <effegi.044377@gmail.com>
2025-07-20 12:48:03 +0200

    Metodo protetto

--- javaScript.js
+++ javaScript.js
@@ -1,36 +1,44 @@
 class Automobile {
-  constructor(marca, modello, anno) {
-    this.marca = marca;
-    this.modello = modello;
-    this.anno = anno;
-    this.chilometraggio = 0;
-  }
+    constructor(marca, modello, anno) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = 0;
+    }
 
-  aggiungiChilometri(km) {
-    this.chilometraggio =+ km;
-  }
+    aggiungiChilometri(km) {
+        this.chilometraggio = + km;
+    }
 
-  mostraChilometraggio() {
-    return `Chilometraggio: ${this.chilometraggio} km`;
-  }
+    mostraChilometraggio() {
+        return `Chilometraggio: ${this.chilometraggio} km`;
+    }
 
-  #calcolaEta() {
-    const annoCorrente = new Date().getFullYear(); 
-    return annoCorrente - this.anno;
-  }
+    #calcolaEta() {
+        const annoCorrente = new Date().getFullYear();
+        return annoCorrente - this.anno;
+    }
 
-  mostraEta() {
-    const eta = this.#calcolaEta(); 
-    return `L'automobile ha ${eta} anni.`;
-  }
+    mostraEta() {
+        const eta = this.#calcolaEta();
+        return `L'automobile ha ${eta} anni.`;
+    }
 
-  descrizione() {
-    return `Questa automobile è una ${this.marca} ${this.modello} del ${this.anno}.`;
-  }
+    _controllaChilometri() {
+        if (this.chilometraggio >= 90000) {
+            return "Attenzione: chilometraggio elevato!";
+        } else {
+            return "Chilometraggio sotto controllo";
+        }
+    }
+
+    descrizione() {
+        return `Questa automobile è una ${this.marca} ${this.modello} del ${this.anno}.`;
+    }
 }
 
 Automobile.prototype.saluta = function () {
-  return `Ciao! Sono una ${this.marca} ${this.modello}.`;
+    return `Ciao! Sono una ${this.marca} ${this.modello}.`;
 };
 
 
@@ -47,9 +55,11 @@ class Elettrica extends Automobile {
     ricarica(km) {
         this.autonomia += km;
     }
+
+    verificaChilometraggio() {
+        return this._controllaChilometri();
+    }
 }
-
-
 
 
 
