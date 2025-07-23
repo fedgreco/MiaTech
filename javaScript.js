a39689fe4075b3ab434c35cc98e375c43a6bad40
fedgreco <effegi.044377@gmail.com>
2025-07-23 10:54:42 +0200

    destructuring con nomi di variabili diversi

--- javaScript.js
+++ javaScript.js
@@ -11,7 +11,7 @@ let persona = {
     city: "Rome"
 }
 
-let {firstName, city} = persona;
+let {firstName: name, city: residenza} = persona;
 
-console.log(firstName);
-console.log(city);
+console.log(name);
+console.log(residenza);
