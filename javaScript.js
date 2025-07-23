a909f94bf491d45be7d9ad3e53cab6dd3fb289f1
fedgreco <effegi.044377@gmail.com>
2025-07-23 10:52:45 +0200

    estrazione di proprietà base

--- javaScript.js
+++ javaScript.js
@@ -3,3 +3,15 @@ let numeri = [1, 2, 3];
 let [uno, due, tre, quattro = 4, cinque = 5] = numeri;
 
 console.log(uno, due, tre, quattro, cinque);
+
+
+let persona = {
+    firstName: "Steve",
+    age: 30,
+    city: "Rome"
+}
+
+let {firstName, city} = persona;
+
+console.log(firstName);
+console.log(city);
