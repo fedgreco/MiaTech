7b2b78da21a638fe706dfad15756226c7b0033cf
fedgreco <effegi.044377@gmail.com>
2025-07-24 14:54:59 +0200

    utilizzare setTimeout

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,4 @@
-let persone = [
-    { nome: "Carlo", età: 26, città: "Milano" },
-    { nome: "Luca", età: 30, città: "Roma" },
-    { nome: "Sara", età: 28, città: "Torino" }
-];
+setTimeout(function () {
+  console.log("Questo messaggio compare dopo 3 secondi!");
+}, 3000);
 
-console.table(persone);
-
-console.group("Messaggi di Debug");
-
-console.log("Avvio dello script...");
-console.log("Array di persone creato");
-console.log("Stampata tabella con console.table");
-console.warn("Controllare che i dati siano corretti");
-console.error("Errore di esempio (solo per dimostrazione)");
-
-console.groupEnd(); 
