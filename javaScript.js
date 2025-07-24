32ed1eecac468c2572b3d619a04ed44ea2fc5e0e
fedgreco <effegi.044377@gmail.com>
2025-07-24 13:26:54 +0200

    stringa multilinea con template literals

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,10 @@
 let nome = "Super";
 let cognome = "Mario";
+let age = 30;
+let city = "Tokyo";
 
-let saluto = `Ciao ${nome} ${cognome}!`;
+let saluto =
+ `Ciao ${nome} ${cognome}!
+ Hai ${age} anni e vivi a ${city}`;
 
 console.log(saluto);
