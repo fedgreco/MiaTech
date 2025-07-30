e1e59698b7c37dab80d61b65a4fe717ab4bfe8e7
fedgreco <effegi.044377@gmail.com>
2025-07-30 11:08:51 +0200

    callback annidati

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,20 @@
-function stampaRisultato(valore) {
-    console.log("Il risultato è: " + valore);
-}
-
-function elaboraNumero(numero, callback) {
+function raddoppiaNumero(numero, callback) {
     const risultato = numero * 2;
     callback(risultato);
 }
 
-elaboraNumero(10, stampaRisultato);
+function aggiungiCinque(numero, callback) {
+    const risultato = numero + 5;
+    callback(risultato);
+}
+
+function stampaRisultato(finale) {
+    console.log("Risultato finale: " + finale);
+}
+
+raddoppiaNumero(4, function (ris1) {
+    aggiungiCinque(ris1, function (ris2) {
+        stampaRisultato(ris2);
+    });
+});
+
