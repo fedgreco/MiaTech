be929d1befa61a63dee98da63fd6554e087de343
fedgreco <effegi.044377@gmail.com>
2025-07-31 14:30:42 +0200

    catena di promise con gestione degli errori

--- javaScript.js
+++ javaScript.js
@@ -1,20 +1,24 @@
-function generaNumero() {
-    return new Promise(function (resolve) {
+function esitoCasuale() {
+    return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            resolve(5);
+            const successo = Math.random() > 0.5;
+            if (successo) {
+                resolve(10); 
+            } else {
+                reject("Errore: la promessa è stata rifiutata.");
+            }
         }, 1000);
     });
 }
 
-generaNumero()
+esitoCasuale()
     .then(function (numero) {
-        console.log("Numero ottenuto:", numero);
-        if (numero % 2 === 0) {
-            return numero * 2;
-        } else {
-            return numero * 3;
-        }
+        console.log("Promise risolta con:", numero);
+        return numero * 2; 
     })
-    .then(function (risultatoFinale) {
-        console.log("Risultato finale:", risultatoFinale);
+    .then(function (risultato) {
+        console.log("Risultato finale:", risultato);
+    })
+    .catch(function (errore) {
+        console.log("Si è verificato un problema:", errore);
     });
