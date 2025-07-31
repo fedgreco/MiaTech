ff3ac1872f21c4b27959ab770dee3c9eb187aac1
fedgreco <effegi.044377@gmail.com>
2025-07-31 14:37:45 +0200

    gestione degli errori con catch

--- javaScript.js
+++ javaScript.js
@@ -1,24 +1,10 @@
-function esitoCasuale() {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            const successo = Math.random() > 0.5;
-            if (successo) {
-                resolve(10); 
-            } else {
-                reject("Errore: la promessa è stata rifiutata.");
-            }
-        }, 1000);
-    });
+function promessaFallita() {
+  return new Promise(function(resolve, reject) {
+      reject("Errore: operazione non riuscita.");
+  });
 }
 
-esitoCasuale()
-    .then(function (numero) {
-        console.log("Promise risolta con:", numero);
-        return numero * 2; 
-    })
-    .then(function (risultato) {
-        console.log("Risultato finale:", risultato);
-    })
-    .catch(function (errore) {
-        console.log("Si è verificato un problema:", errore);
-    });
+promessaFallita()
+  .catch(function(errore) {
+    console.log(errore);
+  });
