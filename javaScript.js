6fd98e32a6f043a08a16cf505a6b609eadcb1994
fedgreco <effegi.044377@gmail.com>
2025-08-01 11:21:36 +0200

    comprendere un errore CORS

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const response = await fetch("http://example.com");
         const data = await response.json();
         console.log("Post ricevuti:", data);
     } catch (error) {
@@ -10,5 +10,6 @@ async function fetchData() {
 
 fetchData();
 
-
+// Access to fetch at 'http://example.com/' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+// CORS - Cross Origin Resourse sharing - è un meccanismo di sicurezza che serve a controllare l'accesso a dati di un dominio differente da quello della pagina di provenienza 
 
