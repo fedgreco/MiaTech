45476fe8a28126e9689e40ecc896d081afb72baf
fedgreco <effegi.044377@gmail.com>
2025-08-01 09:45:24 +0200

    funzioni asincrone in serie

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,26 @@
-function controllaPromise(valido) {
-    return new Promise(function (resolve, reject) {
-            if (valido) {
-                resolve("Operazione riuscita");
-            } else {
-                reject("Si è verificato un errore");
-            }
+function funzioneUno() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve("Risultato della funzioneUno dopo 1 secondo");
+        }, 1000);
     });
 }
 
-async function gestisciPromise(valido) {
-    try {
-        const risultato = await controllaPromise(valido);
-        console.log("Risultato:", risultato);
-    } catch (errore) {
-        console.error("Errore:", errore);
-    }
+function funzioneDue() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve("Risultato della funzioneDue dopo 3 secondi");
+        }, 3000);
+    });
 }
 
-gestisciPromise(true);
-gestisciPromise(false);
+async function eseguiInSerie() {
+    const risultatoUno = await funzioneUno();
+    console.log(risultatoUno);
+
+    const risultatoDue = await funzioneDue();
+    console.log(risultatoDue);
+}
+
+eseguiInSerie();
 
