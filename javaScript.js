39ee1cfa288dcf6cab035c919b0664a884fecb21
fedgreco <effegi.044377@gmail.com>
2025-08-01 11:32:36 +0200

    utilizzare proxy per bypassare CROS

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,17 @@
-async function fetchData() {
+async function fetchConProxy() {
+    const urlOriginale = "https://api.chucknorris.io/jokes/random";
+    const proxyUrl = "https://api.allorigins.win/raw?url=" + encodeURIComponent(urlOriginale);
+
     try {
-        const response = await fetch("http://example.com");
+        const response = await fetch(proxyUrl);
         const data = await response.json();
-        console.log("Post ricevuti:", data);
+        console.log("Battuta di Chuck Norris:", data.value);
     } catch (error) {
-        console.error("Errore nella richiesta GET:", error.message);
+        console.error("Errore con il proxy:", error.message);
     }
 }
 
-fetchData();
+fetchConProxy();
 
-// Access to fetch at 'http://example.com/' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
-// CORS - Cross Origin Resourse sharing - è un meccanismo di sicurezza che serve a controllare l'accesso a dati di un dominio differente da quello della pagina di provenienza 
+
 
