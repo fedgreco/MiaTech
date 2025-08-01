6fa9fedc690af7c33596470589d8052799ffdfb8
fedgreco <effegi.044377@gmail.com>
2025-08-01 11:14:28 +0200

    gestione degli errori con async e await

--- javaScript.js
+++ javaScript.js
@@ -1,20 +1,10 @@
 async function fetchData() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title: "Eseguire una richiesta POST",
-                body: "Scemo chi legge",
-                userId: 1
-            })
-        });
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const data = await response.json();
-        console.log("Dati inviati e risposta ricevuta:", data);
+        console.log("Post ricevuti:", data);
     } catch (error) {
-        console.error("Errore nella fetch POST:", error.message);
+        console.error("Errore nella richiesta GET:", error.message);
     }
 }
 
