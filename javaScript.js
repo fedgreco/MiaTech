8ea036dbd969ec1631b895c1006aca03ea637155
fedgreco <effegi.044377@gmail.com>
2025-08-01 11:11:01 +0200

    eseguire una richiesta POST

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,24 @@
 async function fetchData() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: "Eseguire una richiesta POST",
+                body: "Scemo chi legge",
+                userId: 1
+            })
+        });
         const data = await response.json();
-        console.log("Dati ricevuti:", data);
+        console.log("Dati inviati e risposta ricevuta:", data);
     } catch (error) {
-        console.error("Errore nella fetch:", error.message);
+        console.error("Errore nella fetch POST:", error.message);
     }
 }
 
 fetchData();
+
 
 
