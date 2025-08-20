925459f5da0fdb36f56e874ae3085a1f2d859ed0
fedgreco <effegi.044377@gmail.com>
2025-08-20 18:43:02 +0200

    modifica html

--- javaScript.js
+++ javaScript.js
@@ -3,7 +3,13 @@ const paragrafo = document.querySelector("#paragrafo");
 paragrafo.style.backgroundColor = "red";
 paragrafo.style.fontSize = "30px";
 
-paragrafo.innerText = "Questo è un paragrafo esattamente come lo era prima";
+paragrafo.innerHTML = 'Questo è un paragrafo e ora è comparso un butt(on) --> <button id="button">Non cliccarmi</button>';
+
+const culoSu = document.getElementById("button");
+
+culoSu.addEventListener("click", () => {
+    alert("Ti avevo detto di non cliccare e ora ti becchi il fun fact : ci avevi mai fatto caso che se nella parola button metti uno spazio prima della o diveta letteralmente culo su?")
+})
 
 
 
