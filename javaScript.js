3646ac861ea6f5efe37989c020a22ceef3051072
fedgreco <effegi.044377@gmail.com>
2025-08-20 18:27:02 +0200

    cambia lo stile

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,8 @@
 const paragrafo = document.querySelector("#paragrafo");
 
+paragrafo.style.backgroundColor = "red";
+paragrafo.style.fontSize = "30px";
+
 paragrafo.innerText = "Questo è un paragrafo esattamente come lo era prima";
 
 
