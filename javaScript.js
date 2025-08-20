957a22e870898b6f5910746c82544e9385c8870f
fedgreco <effegi.044377@gmail.com>
2025-08-20 18:02:36 +0200

    local e session storage insieme

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,18 @@
 const setButton = document.querySelector("#set-button");
 const game = document.querySelector("#game");
 
-const gameName = sessionStorage.getItem("my_fav_game") || "";
+const gameName = localStorage.getItem("my_fav_game") || "";
+const gameName1 = sessionStorage.getItem("my_fav_game") || "";
 game.innerText = gameName;
 
 setButton.addEventListener("click", () => {
     const gameName = prompt("Qual è il tuo gioco preferito?");
+    localStorage.setItem("my_fav_game", gameName);
     sessionStorage.setItem("my_fav_game", gameName);
     game.innerText = gameName;
 })
 
+localStorage.removeItem("my_fav_game");
 sessionStorage.removeItem("my_fav_game");
 
 
