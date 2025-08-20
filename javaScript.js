a7483043fa31b6b428dabab87bc0d540c8005fed
fedgreco <effegi.044377@gmail.com>
2025-08-20 18:15:04 +0200

    oggetto document

--- javaScript.js
+++ javaScript.js
@@ -1,19 +1,6 @@
-const setButton = document.querySelector("#set-button");
-const game = document.querySelector("#game");
+const paragrafo = document.querySelector("#paragrafo");
 
-const gameName = localStorage.getItem("my_fav_game") || "";
-const gameName1 = sessionStorage.getItem("my_fav_game") || "";
-game.innerText = gameName;
-
-setButton.addEventListener("click", () => {
-    const gameName = prompt("Qual è il tuo gioco preferito?");
-    localStorage.setItem("my_fav_game", gameName);
-    sessionStorage.setItem("my_fav_game", gameName);
-    game.innerText = gameName;
-})
-
-localStorage.removeItem("my_fav_game");
-sessionStorage.removeItem("my_fav_game");
+paragrafo.innerText = "Questo è un paragrafo";
 
 
 
