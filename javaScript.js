3c82198b9066b02726636036372bd7ba398f052a
fedgreco <effegi.044377@gmail.com>
2025-08-20 17:59:08 +0200

    utilizzare il sessionStorage

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,16 @@
 const setButton = document.querySelector("#set-button");
 const game = document.querySelector("#game");
 
+const gameName = sessionStorage.getItem("my_fav_game") || "";
+game.innerText = gameName;
+
 setButton.addEventListener("click", () => {
     const gameName = prompt("Qual è il tuo gioco preferito?");
-    localStorage.setItem("my_fav_game", gameName);
+    sessionStorage.setItem("my_fav_game", gameName);
     game.innerText = gameName;
 })
 
-const gameName = localStorage.getItem("my_fav_game") || "";
-game.innerText = gameName;
-
-localStorage.removeItem("my_fav_game");
+sessionStorage.removeItem("my_fav_game");
 
 
 
