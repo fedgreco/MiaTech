c6f6e5339256b28114fad80e27701591adb7fd2d
fedgreco <effegi.044377@gmail.com>
2025-08-20 19:00:24 +0200

    crea i list items

--- javaScript.js
+++ javaScript.js
@@ -11,9 +11,17 @@ item2.textContent = "Due";
 const item3 = document.createElement("li");
 item3.textContent = "Tre";
 
+const item4 = document.createElement("li");
+item4.textContent = "Quattro";
+
+const item5 = document.createElement("li");
+item5.textContent = "Cinque";
+
 lista.appendChild(item1);
 lista.appendChild(item2);
 lista.appendChild(item3);
+lista.appendChild(item4);
+lista.appendChild(item5);
 
 container.appendChild(lista);
 
