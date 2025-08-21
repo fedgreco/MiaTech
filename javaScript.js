3fbe6cff16856882684e0fe4b054a3e767060f78
fedgreco <effegi.044377@gmail.com>
2025-08-21 22:45:25 +0200

    submit

--- javaScript.js
+++ javaScript.js
@@ -1,28 +1,15 @@
-const container = document.querySelector("#container");
+const $form = document.querySelector("#form");
+const $nome = document.querySelector("#nome");
+const $cognome = document.querySelector("#cognome");
 
-const lista = document.createElement("ul");
+form.addEventListener("submit", (event) => {
 
-const item1 = document.createElement("li");
-item1.textContent = "Uno";
-
-const item2 = document.createElement("li");
-item2.textContent = "Due";
-
-const item3 = document.createElement("li");
-item3.textContent = "Tre";
-
-const item4 = document.createElement("li");
-item4.textContent = "Quattro";
-
-const item5 = document.createElement("li");
-item5.textContent = "Cinque";
-
-lista.appendChild(item1);
-lista.appendChild(item2);
-lista.appendChild(item3);
-lista.appendChild(item4);
-lista.appendChild(item5);
-
-container.appendChild(lista);
+  if ($nome.value === "" || $cognome.value === "") {
+    alert("Devi compilare tutti i campi!");
+    event.preventDefault();
+  } else {
+    alert("Form inviato correttamente!"); 
+  }
+});
 
 
