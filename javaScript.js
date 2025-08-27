492885330984fcb6a8d0e9731875ac7a4f979dcc
fedgreco <effegi.044377@gmail.com>
2025-08-27 14:11:33 +0200

    installazione e primo utilizzo

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,2 @@
-const $form = document.querySelector("#form");
-const $nome = document.querySelector("#nome");
-const $cognome = document.querySelector("#cognome");
-
-form.addEventListener("submit", (event) => {
-
-  if ($nome.value === "" || $cognome.value === "") {
-    alert("Devi compilare tutti i campi!");
-    event.preventDefault();
-  } else {
-    alert("Form inviato correttamente!"); 
-  }
-});
 
 
