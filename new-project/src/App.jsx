27b0be10cc0c1b712a21ae2b58496ec16e141284
fedgreco <effegi.044377@gmail.com>
2025-09-12 08:56:06 +0200

    contatore semplice pt2/ incrementa contatore / decrementa contatore

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(0);
+
+  const handleAddButton = () => {
+    setCounter((_counter) => {
+      return _counter + 1;
+    })
+  }
+
+  const handleSubButton = () => {
+    setCounter((_counter) => {
+      return _counter - 1;
+    })
+  }
 
   return (
-    <div>Count: 0</div>
+      <div>
+        <button onClick={handleAddButton}>ADD</button>
+        <button onClick={handleSubButton}>SUB</button>
+        <p>
+          Counter: {counter}
+        </p>
+      </div>
   )
 }
 
