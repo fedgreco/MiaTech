4ea6ecbe7e23f4bf917ed979eaf9fdb33b89e2ca
fedgreco <effegi.044377@gmail.com>
2025-09-12 11:34:24 +0200

    creare un form di login

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -2,7 +2,12 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-  
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   const handleAddButton = () => {
     setCounter((_counter) => {
       return _counter + 1;
@@ -25,22 +30,50 @@ const App = () => {
     console.log(event.target.value);
   }
 
+  const handleFormInput = (event) => {
+    const { name, value, type } = event.target;
+
+    setForm((_form) => ({
+      ..._form,
+      [name]: value,
+    }));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(form);
+
+    setForm({
+      email: "",
+      password: "",
+    })
+  }
+
   useEffect(() => {
   }, [counter]);
 
   return (
-      <div>
-        <h1>
-          Titolo che si aggiorna con il valore di counter: {counter}
-        </h1>
-        <button onClick={handleAddButton}>ADD</button>
-        <button onClick={handleSubButton}>SUB</button>
-        <button onClick={handleResetButton}>RESET</button>
-        <p>
-          Counter: {counter}
-        </p>
-        <input type="text" onInput={handleInput}></input>
-      </div>
+    <div>
+
+      <h1>
+        Titolo che si aggiorna con il valore di counter: {counter}
+      </h1>
+      <button onClick={handleAddButton}>ADD</button>
+      <button onClick={handleSubButton}>SUB</button>
+      <button onClick={handleResetButton}>RESET</button>
+      <p>
+        Counter: {counter}
+      </p>
+      <input type="text" onInput={handleInput}></input>
+
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" value={form.email} onInput={handleFormInput} placeholder="Inserisci la tua email"></input>
+        <input type="password" name="password" value={form.password} onInput={handleFormInput} placeholder="Inserisci la tua password"></input>
+        <button type="submit">Submit</button>
+      </form>
+
+    </div>
   )
 }
 
