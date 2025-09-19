057b5b07e4ae7b7e45c5654de5ccc3e92f4173dd
fedgreco <effegi.044377@gmail.com>
2025-09-19 10:08:31 +0200

    usare props.children per il layout pt2

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -3,7 +3,10 @@ import Card from "./components/Card";
 const App = () => {
     return (
         <>
-            <Card />
+            <Card>
+                <h1>Titolo</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dignissimos. Nihil fugiat eum, earum eius enim sint quibusdam mollitia qui.</p>
+            </Card>
         </>
     )
 }
