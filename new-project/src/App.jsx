886fd10755d8712344f66b8e58c39e281b2d9bf2
fedgreco <effegi.044377@gmail.com>
2025-09-25 12:44:37 +0200

    utilizza useFetch in un componente

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,12 +1,25 @@
-import Card from "./components/Card";
+import useFetch from "./components/useFetch";
 
 const App = () => {
+    const {loading, error, data} = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+    if(error) {
+        return (
+            <p>Error: {error}</p>
+        )
+    }
+
+    if(loading) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return (
         <>
-            <Card>
-                <h1>Titolo</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dignissimos. Nihil fugiat eum, earum eius enim sint quibusdam mollitia qui.</p>
-            </Card>
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
         </>
     )
 }
