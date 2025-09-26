6be2370945061c9f422f195c713dfff540d310b3
fedgreco <effegi.044377@gmail.com>
2025-09-26 11:24:05 +0200

    usare un ref per accedere al valore dell'input parte 3

--- new-project/src/components/UncontrolledInput.jsx
+++ new-project/src/components/UncontrolledInput.jsx
@@ -3,9 +3,14 @@ import { useRef } from "react";
 const UncontrolledInput = () => {
     const inputRef = useRef();
 
+    const handleClick = () => {
+        alert(inputRef.current.value);
+    };
+
     return (
         <>
             <input type="text" ref={inputRef} />
+            <button onClick={handleClick}>Input Value</button>
         </>
     )
 }
