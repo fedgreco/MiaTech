26c10d8a6db5cfffbc767c1ff968b7e8db30bf1b
fedgreco <effegi.044377@gmail.com>
2025-10-11 14:27:25 +0200

    aggiungi navigazione tra le pagine

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import ToDoList from "./components/ToDoList";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/NavBar";
 
 const App = () => {
     
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
