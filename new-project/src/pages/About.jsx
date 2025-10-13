3918fa33f5887add6c442f4e5aa871e4dc413da2
fedgreco <effegi.044377@gmail.com>
2025-10-13 10:32:49 +0200

    aggiungi navigazione programmata con useNavigate

--- new-project/src/pages/About.jsx
+++ new-project/src/pages/About.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const About = () => {
+
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate("/");
+    }
+
 
     return (
         <>
             <h1>About</h1>
+            <button onClick={handleGoHome}>Go Home</button>
         </>
     )
 }
