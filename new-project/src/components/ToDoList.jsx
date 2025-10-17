dddd0f605b246b0840d85b72ec7af9d99164687e
fedgreco <effegi.044377@gmail.com>
2025-10-17 11:57:13 +0200

    utilizza useSearchParams per la ricerca

--- new-project/src/components/ToDoList.jsx
+++ new-project/src/components/ToDoList.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useRef, useState } from "react";
 import useFetch from "./useFetch";
 import useFilteredTodos from "./useFilteredTodos";
 import { ToDoContext } from "../context/ToDoContext";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 
 const ToDoList = () => {
     const [searchTodo, setSearchTodo] = useState("");
@@ -10,10 +10,16 @@ const ToDoList = () => {
     // const { loading, error, data } = useFetch("https://jsonplaceholder.typicode.com/todos");
     const [loading, error, data] = useContext(ToDoContext);
     const filteredTodos = useFilteredTodos(data, searchTodo);
+    const [searchParams, setSearchParams] = useSearchParams();
     //const handleSearchChange = useCallback((event) => {
     //  setSearchTodo(event.target.value);
     // }, [setSearchTodo]);
     const params = useParams();
+
+    const handleSearch = (event) => {
+        setSearchTodo(event.target.value);
+        setSearchParams(event.target.value);
+    }
 
     useEffect(() => {
         if (!loading && inputRef.current) { //per poter usare inputRef e far si che il focus sulla barra di ricerca avvenga, loading deve diventare false, ovvero il caricamento dei dati deve terminare e allora input è nel DOM e inputRef.current esiste
@@ -36,7 +42,7 @@ const ToDoList = () => {
 
     return (
         <>
-            <input type="text" onChange={(event) => setSearchTodo(event.target.value)} ref={inputRef} />
+            <input type="text" onChange={handleSearch} ref={inputRef} />
             <ul>
                 {
                     filteredTodos?.map((item) => {
