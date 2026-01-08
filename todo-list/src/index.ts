03a5a88c804dac4d4f4ed974d80cca752d417359
fedgreco <effegi.044377@gmail.com>
2026-01-08 14:51:13 +0100

    aggiungere una funzione per aggiungere un todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,20 @@
 import type { Todo } from "./types";
 
 const todos: Todo[] = [];
+
+const addTodo = (title: string): Todo => {
+  const newTodo: Todo = {
+    id: Date.now(),
+    title,
+    completed: false,
+  };
+
+  todos.push(newTodo);
+
+  return newTodo;
+};
+
+addTodo("ciao");
+console.log(todos);
+addTodo("wella");
+console.log(todos);
