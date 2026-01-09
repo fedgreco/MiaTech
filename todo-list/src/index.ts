2424038ed8c7ba269b111dfc9956760273c1fb3b
fedgreco <effegi.044377@gmail.com>
2026-01-09 11:20:13 +0100

    gestione degli errori con never

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -31,13 +31,17 @@ const getUserTodos = (userId: number): Todo[] => {
     return todos.filter((t) => t.userId === userId);
 };
 
-const t1 = addTodo("ciao");
-const t2 = addTodo("wella");
-const t3 = addTodo("hola");
+// const t1 = addTodo("ciao");
+// const t2 = addTodo("wella");
+// const t3 = addTodo("hola");
 
-assignTodoToUser(t1.id, 1);
-assignTodoToUser(t2.id, 1);
-assignTodoToUser(t3.id, 2);
+// assignTodoToUser(t1.id, 1);
+// assignTodoToUser(t2.id, 1);
+// assignTodoToUser(t3.id, 2);
 
-console.log(getUserTodos(1));
-console.log(getUserTodos(2)); 
+// console.log(getUserTodos(1));
+// console.log(getUserTodos(2)); 
+
+const error = (message: string): never => {
+    throw new Error(message);
+};
